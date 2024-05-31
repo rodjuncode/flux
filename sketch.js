690f5f8abd105f9b6d7a5398fc1100bb8b41f0aa
Rodrigo Junqueira <oi.rodjun@gmail.com>
2024-05-31 19:45:08 -0300

    feat: :sparkles: Adds character

--- sketch.js
+++ sketch.js
@@ -18,18 +18,56 @@ function setup() {
 function draw() {
   background(...palette.bg);
 
-  S.draw(frameCount*2);
+  S.draw(min(frameCount * 2, 2000));
+
+  translate(130, 470);
+  fill(...palette.fg);
+
+  drawCharacter();
+}
+
+function drawCharacter() {
+  let body = new Path2D(
+    "M87.23,234.72C129.71,157.64,73.9.5,73.9.5h101.11s13.33,122,53.33,203.33,99.33,128.67,99.33,128.67H1.68s49.56-32.44,85.56-97.78Z"
+  );
+  drawingContext.fill(body);
+  drawingContext.fill(body);
+  drawingContext.stroke(body);
+
+  translate(72, -60);
+  let chinShadow = new Path2D(
+    "M47.41.03c25.5.6,43.43,15.94,52.51,26.03,4.43,4.93,7.22,11.5,7.98,18.56,3.86,36.2,13.33,159.56-37.3,158.22C21.13,201.51,4.38,77.84.18,36.78-1.1,24.26,4.62,12.16,14.46,6.73,21.73,2.72,32.27-.33,47.41.03Z"
+  );
+  let shadowColor = color(0, 0, 0, 0.7);
+
+  fill(shadowColor);
+  drawingContext.fill(chinShadow);
+
+  fill(...palette.fg);
+  translate(-9, -3);
+  let face = new Path2D(
+    "M121.04,49.93c13.33,191.17-95.55,171.29-120.64-.37C-3.58,22.33,23.1,1.25,50.6.23c34.15-2.95,68.55,22.65,70.44,49.7Z"
+  );
+  drawingContext.fill(face);
+
+  fill(...palette.fg);
+  translate(-1, 42);
+  let chin = new Path2D(
+    "M121.14.44c4.52,43.77,9.44,145.98-43.3,144.76C28.46,144.07,7.9,49.77.49.07"
+  );
+  drawingContext.fill(chin);
+  drawingContext.stroke(chin);
 }
 
 class Spiral {
   constructor() {
-    this.x = scaler.width() / 2;
-    this.y = scaler.height() / 2;
+    this.x = scaler.width() / 2 + 5;
+    this.y = scaler.height() / 2 + 80;
     this.radiusX = 0;
     this.radiusY = 0;
     this.angle = 0;
     this.delta = 3;
-    this.stroke = [palette.fg]; // white
+    this.stroke = [0, 0, 100]; // white
     this.strokeWeight = 1;
 
     this.reset();
